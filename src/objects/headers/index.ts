8a3f07f5b75d0b47487a457a9001848ec7ea7fc2
Arif Budiman <ArifBudiman19@github.com>
2020-04-02 16:52:28 +0700

    modified set depth

--- src/objects/headers/index.ts
+++ src/objects/headers/index.ts
@@ -15,19 +15,24 @@ class Header extends Phaser.GameObjects.Rectangle {
     super(scene, x, y, (width as number), 90, 0x314463);
     this._scene = scene;
     this.setOrigin(0, 0);
-    this.setDepth(1);
-
+    
     this.scoreboard = new Scoreboard(scene, x + this.width * 0.25, y + this.height * 0.25);
-    this.scoreboard.setDepth(this.depth + 1);
-
+    
     this.counter = new Counter(scene, x + 5, y + this.height * 0.25);
-    this.counter.setDepth(this.depth + 1);
-
+    
     this.pausedButton = new PauseButton(scene, x + this.width - 50, y + this.height * 0.5);
     this.pausedButton.setScale(1.2);
-    this.pausedButton.setDepth(this.depth + 1);
-
+    
+    this.setDepth(1);
     scene.add.existing(this);
+  }
+
+  setDepth(value: number) {
+    super.setDepth(value);
+    this.scoreboard && this.scoreboard.setDepth(this.depth + 1);
+    this.counter && this.counter.setDepth(this.depth + 1);
+    this.pausedButton && this.pausedButton.setDepth(this.depth + 1);
+    return this;
   }
 }
 
